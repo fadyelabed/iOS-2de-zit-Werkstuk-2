0e1c0959a05f0c73edbfe0ed0c46a471a0f92ee7
Fady Elabed <fady.elabed@student.ehb.be>
2019-08-14 12:53:42 +0200

    start werkstuk 2

--- Werkstuk2Augustus/ViewController.swift
+++ Werkstuk2Augustus/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //start
         // Do any additional setup after loading the view.
     }
 
